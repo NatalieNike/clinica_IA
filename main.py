03a83366bcd5200f0487032a6da2a966d61d014d
Natalie Nike <silvanatalie72@gmail.com>
2026-05-19 23:37:34 -0300

    subindo versão inicial

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+import joblib
+
+#carregar dataset
+df = pd.read_csv("dataset.csv")
+
+x = df["sintoma"]
+y = df["categoria"]
+
+model = Pipeline([
+    ("vectorizer", TfidfVectorizer()),
+    ("classifier", MultinomialNB())
+])
+
+model.fit(x, y)
+
+joblib.dump(model, "model.pkl")
+
+print("Modelo treinado com sucesso!")
